309e2688962a4087b45e880aa4d11d1167dd543f
Felipe Bittencourt <felipe.bittencourt@ulbra.inf.br>
2019-09-04 13:46:08 -0300

    Add files via upload

---
+++ aulaorientadas/FactoryMethod.java
@@ -0,0 +1,19 @@
+package aula.orientadas;
+public class FactoryMethod {
+
+    public static void main(String[] args) {
+        
+      FactoryFabrica factory = new FactoryFabrica();
+      String modelo = "YEEZY BOOST";
+      String marca = "ADIDAS";
+      factory.getFabricaTenis(marca, modelo);
+      
+      
+      FactoryFabrica factory1 = new FactoryFabrica();
+      String modelo1 = "WAVE PROPHECY 7";
+      String marca1 = "MIZUNO";
+      factory1.getFabricaTenis(marca1, modelo1);
+    }
+    }
+    
+
